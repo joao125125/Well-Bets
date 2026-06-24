01482117d9dced180391a951152a2e2f8c32833e
joão <pato125125@gmail.com>
2026-06-24 10:40:46 -0300

    feat: Criando inicio do java script

---
+++ src/javascript/script.js
@@ -0,0 +1,15 @@
+// DEFINE O OBJETO COM OS RESULTADOS (BANCO DE DADOS)
+const placarOficial ={
+    1:{time1:2,time2:1},
+    2:{time1:3,time2:0},
+}
+// FUNÇÂO SALVAR PALPITE
+function salvarPalpite(nomeJogo,id){
+
+    //CAPTURA OS ELEMENTOS DO DOM
+    const input1 = document.getElementById('time1-${id}');
+    const input1 = document.getElementById('time2-${id}');
+
+    //VALIDAÇÂO DOS CAMPOS
+    
+}
